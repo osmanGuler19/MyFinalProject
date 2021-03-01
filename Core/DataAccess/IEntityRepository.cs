a260c3cd4da80d2520c80ec16883a6018de70888
osmanGuler19 <osman-guler@outlook.com>
2021-03-01 19:01:52 +0300

    Initial commit

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    //SOLID
+    // O : Open Closed Principle
+    //generic constraint
+    //class : referans tip
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        //filter = null filtre vermeyedebilirsin demek
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        
+
+    }
+}
+
+//Code refactoring yaptım. 
+//Core katmanı oluşturup evrensel class'ları burada topladım 
